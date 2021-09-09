b02efc1584c1acc24f51a955a839c6d91d8dbceb
Vetle Roos Mangrud <vmangrud@gmail.com>
2021-09-09 16:06:17 +0200

    Start of code to create random room name

--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -5,15 +5,23 @@
 
     const socket = io("ws://localhost:3001");
 
+    socket.on("newRoom", (name) => {
+        console.log(name);
+    })
+
 </script>
 <header>
     <h1>Headbands</h1>
 </header>
 <main>
-    <RoomMenu on:joinRoom = {evt => {
-        console.log(evt.detail.room);
+    <RoomMenu 
+    on:joinRoom = {evt => {
+        console.log("Joining room: "+evt.detail.room);
         socket.emit("joinRoom", evt.detail.room);
-        console.log(socket);
         
+    }}
+    on:createRoom = {evt => {
+        console.log("Creating room");
+        socket.emit("createRoom");
     }}></RoomMenu>
 </main>
--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
+//Socket.IO
 const io = require("socket.io")(3001 , {
   cors:  {
     origin: 'http://localhost:3000'
   }
 });
 
+//Chance.js
+var Chance = require('chance');
+var chance = new Chance();
+
+//Array storing name of all active rooms
+let activeRooms = [];
 
 io.on('connection', (socket) => {
   console.log('a user connected');
@@ -16,11 +23,20 @@ io.on('connection', (socket) => {
 });
 
 function createRoom(socket) {
-  console.log("Creating room");
-}{
-
+  let name = getNewRoomName();
+  console.log("Creating room " + name);
+  socket.emit("newRoom", name)
 }
 
 function joinRoom(socket, name) {
   console.log("Socket joining " + name);
+}
+
+function getNewRoomName() {
+  let name = chance.word({ length: 6 });
+  while (!activeRooms.includes(name)){
+    name = chance.word({ length: 6 });
+    activeRooms.push(name);
+  }
+  return name;
 }
