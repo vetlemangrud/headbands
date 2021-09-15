f31d87b5c1d00be48507b6cb0abaf3fb60ed36d4
Vetle Roos Mangrud <vmangrud@gmail.com>
2021-09-15 18:04:37 +0200

    "Start Game" button dispatches startGame event

--- client/src/lib/Lobby.svelte
+++ client/src/lib/Lobby.svelte
@@ -1,7 +1,15 @@
 <script>
+    import { createEventDispatcher } from "svelte";
+
+    const dispatch = createEventDispatcher();
+
     export let roomName = "";
     export let members = [];
     export let host = false;
+
+    function startGame(){
+    dispatch("startGame");
+}
 </script>
 <h2>Room {roomName}</h2>
 <h3>Members</h3>
@@ -11,5 +19,5 @@
     {/each}
 </ul>
 {#if host}
-     <button>Start Game</button>
+     <button on:click={startGame}>Start Game</button>
 {/if}
